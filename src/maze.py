aca5beb55690cb5ed2d4d4f48ebe46371539acbc
jn98 <jn98@users.noreply.github.com>
2019-01-16 15:17:20 +1100

    Beginning to build Maze class

--- src/maze.py
+++ src/maze.py
@@ -1,2 +1,21 @@
+import numpy
+
 class Maze:
+    def __init__(self, size, start, finish):
+        self.adj_matrix = numpy.full((size, size), False)
+        self.start = start
+        self.finish = finish
+        self.adj_matrix[0, start] = True
+        self.adj_matrix[size-1, finish] = True
+
+
+
+    def addNode(height, width):
+        adj_matrix[height, width] = True
+            
+"""     class Node: 
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+"""
     
--- src/maze_solver.py
+++ src/maze_solver.py
@@ -1,8 +1,9 @@
 import argparse
 from PIL import Image
-
+from maze import Maze
 
 def main():
+    # example code for inputting input/output of maze.
     '''
     parser = argparse.ArgumentParser()
     parser.add_argument("InputMaze")
@@ -10,27 +11,41 @@ def main():
     args = parser.parse_args()
     print(args.InputMaze)
     '''
+
+    # open given maze.png, if there is an error opening exit.
     try:
-        myimage = Image.open("/media/james/SlowBigMan/MazeSolving/Mazes/Daedelus/3x3.png")
+        myimage = Image.open("/media/james/SlowBigMan/MazeSolving/mazes/Daedelus/3x3.png")
     except:
         print("Error with image")
         exit()
 
+    # Convert image to RGB for path highlighting.
     myimage = myimage.convert("RGB")
     width, height = myimage.size
-    for x in range(width):
-        for y in range(height):
-            r,g,b = myimage.getpixel((x,y)) 
 
+    for x in range(height):
+        # print(x)
+        for y in range(width):
+            r,g,b = myimage.getpixel((y,x)) 
+
+        #put pixel if in first row of pixels or last row of pixels. 
             if r==g==b and r==255:
-                myimage.putpixel((x,y),(51,102,255))
-
-    myimage.save("/media/james/SlowBigMan/MazeSolving/new.png")
+                if (x==0):
+                    start = y
+                    myimage.putpixel((y,x),(51,102,255))
+                elif (x==height-1):
+                    finish = y
+                    myimage.putpixel((y,x),(51,102,255))
+                    
     
-
+    print (height)
+    print( width)
+    print(start)
+    print(finish)
+    maze = Maze(height, start, finish)
+    
+    # Save final image, concude program.
+    myimage.save("/media/james/SlowBigMan/MazeSolving/new.png")
     print("JOB DONE")
-
-
-
 
 main()
