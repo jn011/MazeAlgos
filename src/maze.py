12005ed875c0b261d1aff093d063bc5ea754f505
jn98 <jn98@users.noreply.github.com>
2019-02-26 15:55:06 +1100

    Cleaned up code a bit and added completed isNode function

--- src/maze.py
+++ src/maze.py
@@ -1,21 +1,81 @@
-import numpy
-
+from PIL import Image
 class Maze:
-    def __init__(self, size, start, finish):
-        self.adj_matrix = numpy.full((size, size), False)
+    def __init__(self, height, start, finish,myimage):
+        self.height = height
         self.start = start
         self.finish = finish
-        self.adj_matrix[0, start] = True
-        self.adj_matrix[size-1, finish] = True
+        self.myimage = myimage
 
+    # Checks if a given pixel is a node 
+    # (junction, dead end, corner) excludes start/end
+    def isNode(self, height, width):
+        if self.pixelIsBlack(height, width):
+            return False
+        
+        blackpixelsaround = 0
 
+        if self.aboveIsBlack(height, width):
+            blackpixelsaround = blackpixelsaround + 1
+            if self.leftIsBlack(height, width):
+                return True
+        
+        if self.belowIsBlack(height, width):
+            blackpixelsaround = blackpixelsaround + 1
+            if self.rightIsBlack(height, width):
+                return True
+        
+        if self.leftIsBlack(height, width):
+            blackpixelsaround = blackpixelsaround + 1
+            if self.belowIsBlack(height, width):
+                return True
+        
+        if self.rightIsBlack(height, width):
+            blackpixelsaround = blackpixelsaround + 1
+            if self.aboveIsBlack(height, width):
+                return True
+        
+        # If there are 3 black walls around a
+        # white pixel then there is a dead end
+        if blackpixelsaround == 3:
+            return True
+        
+        # If there are less than or equal to 1 black wall 
+        # around a white pixel then there is a junction
+        if blackpixelsaround <=1:
+            return True
 
-    def addNode(height, width):
-        adj_matrix[height, width] = True
-            
-"""     class Node: 
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-"""
-    
+    # Checks if a given pixel is black
+    def pixelIsBlack(self, height, width):
+        r,g,b = self.myimage.getpixel((width, height))
+        if r==g==b and r==0:
+            return True
+        else:
+            return False
+
+    # Checks if a the pixel above a given pixel is black
+    def aboveIsBlack(self, height, width):
+        if self.pixelIsBlack(height-1, width):
+            return True
+        else:
+            return False
+
+    # Checks if a the pixel below a given pixel is black
+    def belowIsBlack(self, height, width):
+        if self.pixelIsBlack(height+1, width):
+            return True
+        else:
+            return False
+
+    # Checks if a the pixel left a given pixel is black
+    def leftIsBlack(self, height, width):
+        if self.pixelIsBlack(height, width-1):
+            return True
+        else:
+            return False
+
+    # Checks if a the pixel right a given pixel is black
+    def rightIsBlack(self, height, width):
+        if self.pixelIsBlack(height, width+1):
+            return True
+        else:
+            return False
--- src/maze_solver.py
+++ src/maze_solver.py
@@ -14,7 +14,7 @@ def main():
 
     # open given maze.png, if there is an error opening exit.
     try:
-        myimage = Image.open("/media/james/SlowBigMan/MazeSolving/mazes/Daedelus/3x3.png")
+        myimage = Image.open("/media/james/SlowBigMan/MazeSolving/mazes/Daedelus/16x16.png")
     except:
         print("Error with image")
         exit()
@@ -23,26 +23,33 @@ def main():
     myimage = myimage.convert("RGB")
     width, height = myimage.size
 
-    for x in range(height):
-        # print(x)
-        for y in range(width):
-            r,g,b = myimage.getpixel((y,x)) 
-
-        #put pixel if in first row of pixels or last row of pixels. 
+    for y in range(width):
+            r,g,b = myimage.getpixel((y,0)) 
+        # Set pixel as start if white in first row of pixels
             if r==g==b and r==255:
-                if (x==0):
                     start = y
-                    myimage.putpixel((y,x),(51,102,255))
-                elif (x==height-1):
+                    myimage.putpixel((y,0),(51,102,255))
+
+    for y in range(width):
+            r,g,b = myimage.getpixel((y,height-1)) 
+        # Set pixel as finish if white in last row of pixels. 
+            if r==g==b and r==255:
                     finish = y
-                    myimage.putpixel((y,x),(51,102,255))
+                    myimage.putpixel((y,height-1),(51,102,255))
                     
     
-    print (height)
-    print( width)
+    print(height)
+    print(width)
     print(start)
     print(finish)
-    maze = Maze(height, start, finish)
+    maze = Maze(height, start, finish, myimage)
+
+    # Loop through image and find all nodes except for start/end
+    for x in range(1, height-1):
+        for y in range(width):
+            if maze.isNode(x,y):
+                myimage.putpixel((y,x),(51,102,255))
+
     
     # Save final image, concude program.
     myimage.save("/media/james/SlowBigMan/MazeSolving/new.png")
